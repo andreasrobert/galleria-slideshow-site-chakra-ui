1e6fadcc77a61c20395334a376a83e16f50ffbfa
andreasrobert <andreasrobb23@gmail.com>
2021-07-29 23:47:15 +0700

    skeleton 02

---
+++ components/home.tsx
@@ -0,0 +1,15 @@
+import { Grid, Box } from "@chakra-ui/react";
+
+function Home() {
+  return (
+    <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+      <Box w="100%" h="10" bg="blue.500" />
+      <Box w="100%" h="10" bg="blue.500" />
+      <Box w="100%" h="10" bg="blue.500" />
+      <Box w="100%" h="10" bg="blue.500" />
+      <Box w="100%" h="10" bg="blue.500" />
+    </Grid>
+  );
+}
+
+export default Home;
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,13 +1,12 @@
 import { Flex, Container } from '@chakra-ui/react';
 import Header from '../components/header';
-import Home from '../components/home';
-
+import Slide from '../components/slide'
 export default function HomePage() {
   return (
     <>
       <Container bg="darkcyan" minH="100" maxW="1440" p="20px 40px 30px 40px">
         <Header></Header>
-        <Home></Home>
+        <Slide></Slide>
       </Container>
     </>
   )
