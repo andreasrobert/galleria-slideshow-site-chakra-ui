83c1ce1a1a7f3f40a2ab60fd57c096916ce30776
andreasrobert <andreasrobb23@gmail.com>
2021-08-03 13:44:13 +0700

    router query done

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,39 +1,56 @@
 import { Flex, Container, Box, BoxProps } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Slide from "../components/slide";
 import SlideBar from "../components/slideBar";
 import { motion, AnimatePresence } from "framer-motion";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import Focus from "../components/focus";
 
-export const getServerSideProps: GetServerSideProps = async(ctx) =>{
-  let start = 0
-  if(ctx.query.foo){
-    console.log("helo" + ctx.query.foo)
-  }
-  return {
-    props:{start}
-  }
-  // const router = useRouter()
-  // const  pid  = router.query
-  // let start = 0
-  // if(Number(pid.foo)>=0 && Number(pid.foo) <=14){
-  //   console.log("Hello")
-  //   start = Number(pid.foo)
-  // }
-}
+// export const getServerSideProps: GetServerSideProps = async(ctx) =>{
+//   let start = 0
+//   if(ctx.query.foo){
+//     console.log("helo" + ctx.query.foo)
+//   }
+//   return {
+//     props:{start}
+//   }
+//   // const router = useRouter()
+//   // const  pid  = router.query
+//   // let start = 0
+//   // if(Number(pid.foo)>=0 && Number(pid.foo) <=14){
+//   //   console.log("Hello")
+//   //   start = Number(pid.foo)
+//   // }
+// }
 
 export default function Gallery() {
+  const router = useRouter();
+  const pid = router.query;
 
-  const [popUp, setPopUp] = useState(false)
+  // console.log(start);
 
-  const handlePopUp = () =>{
-    setPopUp(prev => !prev);
-};
-  
-  const [[slide, direction], setSlide] = useState([0, 0]);
+  const [popUp, setPopUp] = useState(false);
+
+  const handlePopUp = () => {
+    setPopUp((prev) => !prev);
+  };
+
+  const [[slide, direction], setSlide] = useState([-999, 0]);
+
+  useEffect(() => {
+    const start = (() => {
+      if (Number(pid.foo) >= 0 && Number(pid.foo) <= 14) {
+        console.log("Hello", pid.foo);
+        return Number(pid.foo);
+      }
+      return 0;
+    })();
+
+    setSlide([start, 0]);
+  }, [pid.foo])
+
 
   const paginate = (newDirection: number) => {
     setSlide([slide + newDirection, newDirection]);
@@ -45,22 +62,25 @@ export default function Gallery() {
   if (slide >= 15) {
     setSlide([0, direction]);
   }
+  if (slide === -999) {
+    return null;
+  }
 
   return (
     <>
-        <Focus slide={slide} handlePopUp={handlePopUp} popUp={popUp}></Focus>
+      <Focus slide={slide} handlePopUp={handlePopUp} popUp={popUp}></Focus>
 
       <Container
-        overflow={{base:"auto",sm:"hidden"}}
+        overflow={{ base: "auto", sm: "hidden" }}
         bg="white"
         pos="absolute"
         left="0"
         right="0"
         minH="100%"
         h="100%"
-        minW={{base:"100%", xl:"1440"}}
+        minW={{ base: "100%", xl: "1440" }}
         maxW="152vh"
-        p={{base:"20px", mm:"2.1vh 40px 0 40px"}}
+        p={{ base: "20px", mm: "2.1vh 40px 0 40px" }}
         d="flex"
         flexDir="column"
         justifyContent="space-between"
@@ -68,41 +88,39 @@ export default function Gallery() {
         <Header show={true}></Header>
         <AnimatePresence initial={false} custom={direction}>
           <Box>
-        <motion.div
-        key={slide}
-        custom={direction}
-        variants={variants}
-        initial="enter"
-        animate="center"
-        exit="exit"
-        transition={{
-          x: { type: "spring", stiffness: 300, damping: 30 },
-          opacity: { duration: 0.2 }
-        }}
-        drag="x"
-        dragConstraints={{ left: 0, right: 0 }}
-        dragElastic={1}
-        onDragEnd={(e, { offset, velocity }) => {
-          const swipe = swipePower(offset.x, velocity.x);
+            <motion.div
+              key={slide}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
 
-          if (swipe < -swipeConfidenceThreshold) {
-            paginate(1);
-          } else if (swipe > swipeConfidenceThreshold) {
-            paginate(-1);
-          }
-        }}
-        >
-            <Slide slide={slide} handlePopUp={handlePopUp}></Slide>
-        </motion.div>
-        
-        </Box>
-      </AnimatePresence>
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
+            >
+              <Slide slide={slide} handlePopUp={handlePopUp}></Slide>
+            </motion.div>
+          </Box>
+        </AnimatePresence>
         <SlideBar paginate={(id) => paginate(id)} slide={slide}></SlideBar>
       </Container>
     </>
   );
 }
-
 
 const variants = {
   enter: (direction: number) => {
